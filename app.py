0164c38c18436fc36b94ccdae8d2c2b192c633eb
gour aman <127648041+amangour7909@users.noreply.github.com>
2024-12-27 11:01:29 +0530

    login register page added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template,url_for
-
+from flask import Flask,render_template,url_for,redirect
+from forms import RegistrationForm, LoginForm
+from flask import flash
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -25,6 +28,22 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
