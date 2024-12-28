55e6765b17a7c06e48f5d73f823d492e5754250a
gour aman <127648041+amangour7909@users.noreply.github.com>
2024-12-28 22:47:35 +0530

    login logout register implemented correctly with db setup

---
+++ flaskblog/routes.py
@@ -0,0 +1,73 @@
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+from flaskblog import db
+import bcrypt
+from flask_login import login_user,current_user,logout_user,login_required
+
+posts = [
+    {
+        'author':'Corey Schafer',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'April 20, 2018'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.hashpw(form.password.data.encode('utf-8') , bcrypt.gensalt())
+
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.checkpw(form.password.data.encode('utf-8'), user.password):
+            login_user(user)
+            flash('Login Successful', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
--- readme.md
+++ readme.md
@@ -15,3 +15,20 @@ what is cdn? css and js uses it means we need not to download anything
 create forms using wtforms apply validators, error handling
 
 always use url_for for linking webpages
+
+# 4. database implementation
+use sqlalchemy - here we can represent database structure as classes
+
+set uri's for database , create its instance then define each table as class
+
+ --> test database creation using python interpretor it will create site.db file in same directory 
+ --> also test inserting a user and post and then fetch the records
+
+
+# 5. Convert application into package structure
+    error - circular import error
+    app.app_context().push() -- run this command to create db from console
+    handle duplicate use check before submitting form
+
+# 6. install flask login mangager for handling login
+    
